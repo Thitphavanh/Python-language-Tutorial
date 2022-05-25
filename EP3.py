5327aaed44a980071968998f91c64718f157387b
Hery <thitphavanh.psv@gmail.com>
2022-05-25 08:20:28 +0700

    update

---
+++ EP3.py
@@ -0,0 +1,12 @@
+# String => ຂໍ້ຄວາມ ອັກສອນ
+# number => Integer, float
+# Boolean => True, False
+
+print('Hery')
+print(-1)
+print(0)
+print(1)
+print(3.55)
+print(True)
+print(False)
+print("Hery \n" + "Engineer")
