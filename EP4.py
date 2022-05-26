fa3cecc32ed9e262da7e3dfd44992fb609975e0c
Hery <thitphavanh.psv@gmail.com>
2022-05-26 07:18:55 +0700

    update

--- EP4.py
+++ EP4.py
@@ -1,1 +1,12 @@
 # Data type & Variable
+
+# ຊື່ໂຕແປ = ຄ່າທີ່ເກັບໃນໂຕແປ
+x = 10
+y = 3.10
+z = True
+print('Result =', x)
+print('Result' +str(x)) # ແປງເປັນ String ໂດບໃຊ້ str
+print(y)
+print(z)
+print(type(x)) # ກວດສອບຊະນິດໂຕແປ x = <class 'int'>
+print(type(y)) # ກວດສອບຊະນິດໂຕແປ y = <class 'float'>
