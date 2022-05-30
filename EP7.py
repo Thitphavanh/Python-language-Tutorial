3d61227a7a1dd075d1c4f3deb912cbe41cc77b37
Hery <thitphavanh.psv@gmail.com>
2022-05-30 08:07:05 +0700

    update

---
+++ EP7.py
@@ -0,0 +1,15 @@
+# funtion input
+# fname = input("Please fill your firstname : ")
+# lname = input("Please fill your lastname : ")
+# print("Your firstname is = " + fname)
+# print("Your lastname is = " + lname)
+
+# input
+x = int(input("Please fill number No.1 : "))
+y = int(input("Please fill number No.2 : "))
+
+# prccess & output
+print(x+y)
+
+
+
