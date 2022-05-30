f2ba2a04a185c591b6e816b151917201e7a79929
Hery <thitphavanh.psv@gmail.com>
2022-05-30 08:15:06 +0700

    update

--- EP8.py
+++ EP8.py
@@ -6,3 +6,14 @@
 # A + B 
 # A, B => Oerand
 #  + => Operator
+
+x = 5
+y = 2
+
+print("ຜົນບວກ =", x + y)
+print("ຜົນລົບ =", x - y)
+print("ຜົນຫານ =", x / y)
+print("ຜົນຫານປັດເສດ =", x // y)
+print("ຜົນຫານເອົາເສດ =", x % y)
+print("ຜົນຄູນ =", x * y)
+print("ຜົນຍົກກຳລັງ =", x ** y)
