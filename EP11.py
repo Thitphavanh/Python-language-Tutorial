a86b40e225e9747252b62a59b0ffda1d0cd9a403
Hery <thitphavanh.psv@gmail.com>
2022-06-01 06:44:50 +0700

    update

---
+++ EP11.py
@@ -0,0 +1,12 @@
+# Compound Assignment
+x = 10
+print('ກ່ອນເພີ່ມຄ່າ', x)
+
+x += 5 # x = x + 5 
+x -= 15 # x = x - 5 
+x *= 5 # x = x * 5 
+x /= 5 # x = x / 5 
+x //= 5 # x = x // 5 
+x **= 5 # x = x ** 5 
+x %= 4 # x = x % 5 
+print('ຫຼັງເພີ່ມຄ່າ', x)
