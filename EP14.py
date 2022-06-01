797b33ccab7f792d2d179f2109f1ec9918af6ada
Hery <thitphavanh.psv@gmail.com>
2022-06-02 06:29:30 +0700

    update

---
+++ EP14.py
@@ -0,0 +1,22 @@
+'''
+ໂຄງສ້າງຄວບຄຸມ (Controll structure)
+ແບບລຳດັບ
+ແບບເລືອກ
+ແບບເຮັດຊ້ຳ
+'''
+
+'''
+if boolean expression:
+    statement
+'''
+age = int(input('Please fill your age : '))
+name = 'hery'
+
+# print(type(age==15))
+# print(name=='hery')
+
+if age >= 15:
+    print('ຄຳນຳໜ້າເປັນ ທ້າວ/ນາງ')
+    print('ຈົບໂປຣແກຣມທາງໃນ if')
+
+print('ຈົບໂປຣແກຣມ')
