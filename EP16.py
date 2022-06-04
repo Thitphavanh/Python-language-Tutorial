51c9897349a5a87a9f6d0722d9674f9160b9574b
Hery <thitphavanh.psv@gmail.com>
2022-06-04 08:26:01 +0700

    update

--- EP16.py
+++ EP16.py
@@ -2,14 +2,22 @@
 
 age = int(input('Please fill your age : '))
 
-if age <= 15:
-    if age == 15:
-        print('ມ.4')
+if age <= 17:
+    if age == 17:
+        print('ມ.7')
+    elif age ==16:
+        print('ມ.6')
+    elif age ==15:
+        print('ມ.5')
     elif age ==14:
-        print('ມ.3')
+        print('ມ.4')
     elif age ==13:
-        print('ມ.2')
+        print('ມ.3')
     elif age ==12:
+        print('ມ.2')
+    elif age ==11:
         print('ມ.1')
+    else:
+        print('ປະຖົມ')
 else:
     print('ມ.ປາຍ')
