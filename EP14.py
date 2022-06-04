78ee70b45de66d56257a7b7e867b11cf44e9f2ec
Hery <thitphavanh.psv@gmail.com>
2022-06-04 08:12:56 +0700

    update

--- EP14.py
+++ EP14.py
@@ -20,12 +20,30 @@ name = 'hery'
 # print(type(age==15))
 # print(name=='hery')
 
+# if age >= 15 and age <= 20:
+#     print('ເປັນໄວລູ້ນ')
+# elif age >= 20 and age <= 29:
+#     print('ເປັນໜຸ່ມສາວ')
+# elif age >= 30 and age <= 39:
+#     print('ເປັນຜູ້ໃຫຍ່ໄວເຮັດວຽກ')
+# elif age >= 40 and age <= 69:
+#     print('ເປັນຜູ້ໃຫຍ່')
+# elif age >= 70 and age <= 90:
+#     print('ເປັນຜູ້ເຖົ້າ')
+# else:
+#     print('ເປັນເດັກນ້ອຍ')
+
+# print('ຈົບໂປຣແກຣມ')
+
+# 15 - 20 => ໄວລູ້ນ
+# 21 - 29 => ຜູ້ໃຫຍ່
+# 31 - 39 => ຜູ້ໃຫຍ່ໄວເຮັດວຽກ
+
+# and or not
+
+
 if age >= 15:
     print('ເປັນໄວລູ້ນ')
-elif age >= 20:
-    print('ເປັນຜູ້ໃຫຍ່')
-elif age >= 30:
-    print('ເປັນຜູ້ໃຫຍ່ໄວເຮັດວຽກ')
 else:
     print('ເປັນເດັກນ້ອຍ')
 
