09d294a7a7a9a13a482822a9ff7fa2a1030caaf9
Hery <thitphavanh.psv@gmail.com>
2022-06-08 07:09:20 +0700

    update

--- EP22Assignment.py
+++ EP22Assignment.py
@@ -1,8 +1,28 @@
-# ແປງ ຄ.ສ ເປັນ ພ.ສ 
-# ແປງ ພ.ສ ເປັນ ຄ.ສ
+# ໂປຣແມຣມຄຳນວນ BMI
 
-number = int(input('ປ້ອນ ພ.ສ : '))
 
-number=number-543
+weight = int(input('ປ້ອນນ້ຳໜັກຂອງເຈົ້າ (kg) : '))
+high = int(input('ປ້ອນນ້ຳໜັກຂອງເຈົ້າ (cm) : '))/100
 
-print('ເປັນ ຄ.ສ =',number)
+bmi = weight/(high**2)
+
+
+if bmi < 18.0:
+    result = 'ຈ່ອຍ'
+
+elif bmi >= 18.5 and bmi <= 22.9:
+    result = 'ສົມສ່ວນ'
+
+elif bmi >= 23.0 and bmi <= 24.9:
+    result = 'ນ້ຳໜັກເກີນ'
+
+elif bmi >= 25.0 and bmi <= 29.9:
+    result = 'ໂລກອ້ວນ ລະດັບ 1'
+
+elif bmi > 30:
+    result = 'ໂລກອ້ວນ ລະດັບ ອັນຕະລາຍ'
+
+else:
+    result = 'ບໍ່ຮູ້ຄ່າທີ່ຊັດເຈນ'
+
+print('ຄ່າ bmi = ', bmi, 'ຄາດການ = ', result)
