29869e07c4aa28ee3f3a400a39cff8ce286dfa3e
Hery <thitphavanh.psv@gmail.com>
2022-06-08 07:12:38 +0700

    update

--- EP22Assignment.py
+++ EP22Assignment.py
@@ -7,22 +7,16 @@ high = int(input('ປ້ອນນ້ຳໜັກຂອງເຈົ້າ (cm) : 
 bmi = weight/(high**2)
 
 
-if bmi < 18.0:
+if bmi >= 0 and bmi < 18.0:
     result = 'ຈ່ອຍ'
-
 elif bmi >= 18.5 and bmi <= 22.9:
     result = 'ສົມສ່ວນ'
-
 elif bmi >= 23.0 and bmi <= 24.9:
     result = 'ນ້ຳໜັກເກີນ'
-
 elif bmi >= 25.0 and bmi <= 29.9:
     result = 'ໂລກອ້ວນ ລະດັບ 1'
-
 elif bmi > 30:
     result = 'ໂລກອ້ວນ ລະດັບ ອັນຕະລາຍ'
-
 else:
     result = 'ບໍ່ຮູ້ຄ່າທີ່ຊັດເຈນ'
-
 print('ຄ່າ bmi = ', bmi, 'ຄາດການ = ', result)
