f61e47985fcc45c8b139cee426af71a63432b7f3
Hery <thitphavanh.psv@gmail.com>
2022-06-11 04:39:30 +0700

    update

--- EP23.py
+++ EP23.py
@@ -1,4 +1,14 @@
-# ການເຂົ້້າເຖິງໂຕອັກສອນໃນ String
+# 1 ການເຂົ້້າເຖິງໂຕອັກສອນໃນ String
+# 2 len function
+# 3 ລຶບຊ່ຶງຊ້າຍຂວາ (strip)
+# 4 ແປງ case ຂອງ string
+# 5 ການແທນທີ່
+# 6 ການກວດສອບຂໍ້ຄວາມ => True, False
+# 7 ການຕໍ່ String (Concatinate) +
+# 8 ການຈັດຮູບແບບການສະແດງຜົນ {}
+# 9 ນັບຈຳນວນຄຳໃນປະໂຫຍກ
+# ກວດສອບຄຳຂຶ້ນຕົ້ນ / ກວດສອບຄຳລົງທ້າຍ
+
 # name = '  hery phenomenal : 27  '
 
 # # ນັບ index
@@ -24,10 +34,45 @@
 # # print(name.capitalize())
 # print(name.replace('4','3.5',1))
 
-name = 'ໄປຊືເຂົ້າ ແລະ ອາຫານຢູ່ຕະຫຼາດ'
+# 7
+# name = 'ໄປຊືເຂົ້າ ແລະ ອາຫານຢູ່ຕະຫຼາດ'
 
-x = 'ເຂົ້າ' not in name
-if x:
-    name = name.replace('ເຂົ້າ','ເປັດ')
+# x = 'ເຂົ້າ' not in name
+# if x:
+#     name = name.replace('ເຂົ້າ','ເປັດ')
 
-print(name)
+# print(name)
+
+
+# 8
+# fnam = 'Hery'
+# lname = 'Phongsavath'
+# age = '24'
+# employment = 'Programmer'
+# salary = 1500000.124545
+
+# text = ('ຊື່ຂຶ້ນຕົ້ນ : {}, ນາມສະກຸນ : {}, ອາຍຸ : {}, ອາຊີບ {}, ເງິນເດືອນ {:,.2f}')
+
+# print(text.format(fnam, lname, age, employment, salary))
+
+# 9
+# market = 'I went to the market, buy some milk, bread and after to meet friends'
+
+# print(market.count('went'))
+
+
+# 10
+# name = 'Mrs. Nang'
+
+# # print(name.startswith('Mr.'))
+# if name.startswith('Mr.'):
+#     print('Men')
+# else:
+#     print('Women')
+
+name = '7788'
+
+if name.endswith('8'):
+    print('Lucky number')
+else:
+    print('not good')
