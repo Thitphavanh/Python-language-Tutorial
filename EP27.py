ca7b9a62f5e605e805b38c47aef91a8298efe152
Hery <thitphavanh.psv@gmail.com>
2022-06-20 05:42:01 +0700

    update

--- EP27.py
+++ EP27.py
@@ -1,9 +1,11 @@
 # ໂຄງສ້າງຄວບຄູມແບບເຮັດຊ້ຳ
 
 summation = 0
-for i in range(1, 5):
+for i in range(1, 11):
     summation += i
     print('ຮອບທີ =', i, 'Sum =', summation)
 
 
 print('Total =', summation)
+print('Average =', summation/5)
+
