3cf204945db9bfd4bc471ac762aba3255844a660
Hery <thitphavanh.psv@gmail.com>
2022-06-20 05:40:48 +0700

    update

--- EP26.py
+++ EP26.py
@@ -9,10 +9,14 @@ i = 1
 summation = 0
 average = 0
 
-while i <= 5:
+count = int(input('Sprint number :'))
+
+while i <= count:
     summation += i
     print('ຮອບທີ =', i, 'ຄ່າ sum = ', summation)
     i += 1
-average = summation/15
+
+
+average = summation/count
 print('ຜົນລວມຂອງການບວກເລກ =', summation)
 print('ຄ່າສະເລ່ຍ =', average)
---
+++ EP27.py
@@ -0,0 +1,9 @@
+# ໂຄງສ້າງຄວບຄູມແບບເຮັດຊ້ຳ
+
+summation = 0
+for i in range(1, 5):
+    summation += i
+    print('ຮອບທີ =', i, 'Sum =', summation)
+
+
+print('Total =', summation)
