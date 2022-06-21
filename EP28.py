4ebeb3eb9b75c19f7a44d2c025b8829b8d509777
Hery <thitphavanh.psv@gmail.com>
2022-06-22 06:11:01 +0700

    update

--- EP28.py
+++ EP28.py
@@ -5,7 +5,7 @@ i = 1
 while i <= 3:
     j = 1
     while j <= 5:
-        print('Sprint number = ', i, 'Location number = ', j)
+        print('ຮອບທີ = ', i, 'ຕຳແໜ່ງທີ່ = ', j)
         j += 1
     i += 1
     
@@ -13,6 +13,6 @@ print('---------------------')
 
 # for loop
 for i in range(1,4):
-    print('Sprint number = ', i)
+    print('ຮອບທີ = ', i)
     for j in range(1,6):
-        print('Location number = ', j) 
+        print('ຕຳແໜ່ງທີ່ = ', j) 
---
+++ EP30.py
@@ -0,0 +1,10 @@
+# break / continue
+
+i = 1
+while i <= 10:
+    print('ຮອບທີ =', i)
+    if(i == 5):
+        break
+    i += 1
+else:
+    print('ຈົບໂປຮແກຣມ')
