a3e27bba3ac72b675fbe03e699d092edb17b9b1c
Hery <thitphavanh.psv@gmail.com>
2022-06-22 06:36:04 +0700

    update

--- EP32Assignment.py
+++ EP32Assignment.py
@@ -1,6 +1,24 @@
 sum = 0
 
+'''
 while sum != 100:
     number = int(input('ປ້ອນໂຕເລກຂອງເຈົ້າ :'))
     sum += number
+    print('ຜົນລວມ =', sum)
+'''
+
+'''
+while sum <= 100:
+    number = int(input('ປ້ອນໂຕເລກຂອງເຈົ້າ :'))
+    sum += number
+    print('ຜົນລວມ =', sum)
+
+'''
+
+
+while True:
+    number = int(input('ປ້ອນໂຕເລກຂອງເຈົ້າ :'))
+    sum += number
+    if sum>=100:
+        break
     print('ຜົນລວມ =', sum)
