2781ec6bbef570c9f8d0a80047f56eb73ea34e24
Hery <thitphavanh.psv@gmail.com>
2022-06-23 07:30:55 +0700

    update

--- EP34Assignment.py
+++ EP34Assignment.py
@@ -2,5 +2,7 @@
 
 last = int(input('ປ້ອນໂຕເລກ : '))
 
-for row in range(1, last):
-    print(row)
+for row in range(1, last+1):
+    for column in range(1, row+1):
+        print(column, end='')
+    print(' ')
