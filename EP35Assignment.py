c62addd82a385c0ca6f7e35d1e3337035624edf0
Hery <thitphavanh.psv@gmail.com>
2022-06-24 05:59:21 +0700

    update

---
+++ EP35Assignment.py
@@ -0,0 +1,8 @@
+# ສ້າງຮູບແຕ້ມ 4 ຫຼ່ຽມຈະຕຸລັດ
+
+number = int(input('ປ້ອນຂະໜາດ = '))  # 5 => 0,1,2,3,4
+
+for row in range(number):
+    for column in range(number):
+        print('x',end='')
+    print(' ')
