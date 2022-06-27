94a8b729d2c83650d73f01210b276638cc6a1c4f
Hery <thitphavanh.psv@gmail.com>
2022-06-27 07:06:21 +0700

    update

---
+++ Python Phase2/EP1.py
@@ -0,0 +1,15 @@
+# Primitive
+
+a = 1
+a1 = 2
+a2 = 3
+a3 = 4
+
+# Non primitive : list
+number = []
+number = [1, 2, 3, 4]
+name = ['Red', 'Blue', 'Yellow', 'Green']
+allList = [4, 'Blue', True, 0.5, -10]
+
+# constructor
+name = list(['Red', 'Blue', 'Yellow', 'Green'])
