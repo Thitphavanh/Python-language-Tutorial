dea99b8ed403c57eb955e2d76e7841785b0227ad
Hery <thitphavanh.psv@gmail.com>
2022-07-02 06:33:07 +0700

    update

--- Python Phase2/EP19.py
+++ Python Phase2/EP19.py
@@ -1,21 +1,9 @@
-# Assignment ຫາເລກຄູ່ / ເລກຄີກ 
-number = []
+# ລຽງລຳດັບ
+student = ['ເຈມສ໌', 'ນ້ອຍ', 'ຕູ້ຍ', 'ໂຈ້', 'ເລ', 'ເລ້']
 
-# ເລກຄີກ
-odd = []
-# ເລກຄູ່
-even = []
 
-while True:
-    x = int(input('ປ້ອນໂຕເລກຂອງເຈົ້າ : '))
-    if x < 0:
-        break
-    if x % 2 == 0:
-        even.append(x)
-    else:
-        odd.append(x)
-    number.append(x)
-
-print('Total number ->', number)
-print('ເລກຄີກ ->', odd)
-print('ເລກຄູ່ ->', even)
+print(student)
+student.sort()
+print(student)
+student.reverse()
+print(student)
