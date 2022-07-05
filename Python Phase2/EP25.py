15ff62b6023a7643f8483e2c351a90ea5b2ea631
Hery <thitphavanh.psv@gmail.com>
2022-07-06 06:43:23 +0700

    update

--- Python Phase2/EP25.py
+++ Python Phase2/EP25.py
@@ -1,8 +1,7 @@
 # tuple
-tups = tuple((1, 2, 3, 4, 'Hery', 'cat', 'dog'))
-lists = list([1, 2, 3, 4, 'Hery', 'cat', 'dog', True, 3.14])
+tups = tuple((1, 2, 3, 4, 'Hery', 'cat', 'dog', True, 3.14))
+# lists = list([1, 2, 3, 4, 'Hery', 'cat', 'dog', True, 3.14])
 
-lists[0] = 'Savannakhet'
-tups[0] = 'Savannakhet'
-print(tups)
-print(lists)
+# lists[0] = 'Savannakhet'
+print(tups[4])
+
