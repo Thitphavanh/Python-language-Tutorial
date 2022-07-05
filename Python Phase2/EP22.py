de8340c29057169fede1c063719e858fc3fb730f
Hery <thitphavanh.psv@gmail.com>
2022-07-05 07:01:06 +0700

    update

--- Python Phase2/EP22.py
+++ Python Phase2/EP22.py
@@ -1,2 +1,15 @@
 # ຈັບຄູ່ຄຳທັກທາຍ / ບຸກຄົນ
 
+greeting = ['ສະບາຍດີ', 'Hi', 'Hello', 'Good bye']
+people = ['Jo', 'James', 'Noy']
+result = []
+
+# ແບບປົກກະຕິ
+for x in greeting:
+    for y in people:
+        result.append(x+y)
+# print(result)
+
+# ແບບຫຼຸດຮູບ
+result = [x+y for x in greeting for y in people]
+print(result)
---
+++ Python Phase2/EP23.py
@@ -0,0 +1,7 @@
+# ຈັບຄູ່ສິນຄ້າ ແລະ ລາຄາ
+
+foods = ['humbuger', 'pizza', 'spargetti']
+price = [50000, 69000, 60000]
+
+for x, y in zip(foods, price):
+    print('ສິນຄ້າ :', x, 'ລາຄາ :', y, 'ກີບ')
