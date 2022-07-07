493c0bf3ae5647b89228f288a53010acfc96aea4
Hery <thitphavanh.psv@gmail.com>
2022-07-07 07:57:26 +0700

    update

--- Python Phase2/EP29.py
+++ Python Phase2/EP29.py
@@ -1,5 +1,8 @@
 # tuple len member
 tups = tuple((1, 2, 3, 4, 'Hery', 'cat', 'dog', True, 3.14))
 
-x = ('Hery')
+names = ('James', 'Jo')
+new = ('Noy', 'Nit')
 
+all_new_names = names + new
+print(all_new_names)
