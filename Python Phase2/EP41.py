5215f0c13c291e855977a23164a56aafc3ffb81b
Hery <thitphavanh.psv@gmail.com>
2022-07-27 06:58:00 +0700

    update

--- Python Phase2/EP41.py
+++ Python Phase2/EP41.py
@@ -5,13 +5,41 @@ fruit = {'ໝາກຂາມ', 'ໝາກມ່ວງ', 'ໝາກຍົມ', 3.
 fruit.add('ໝາກທຸລຽນ')
 fruit.add('ໝາກນັດ')
 fruit.add('ໝາກຂຽບ')
+fruit.add('2000')
+lists = ['ຫົວສີໄຄ', 'ຜັກຂະແຍງ', 'ຜັກຂະ']
+
+# add more elememt
+fruit.update(lists)
+
+for f in fruit:
+    print('information', f)
+
+# show number in set element
+print(len(fruit))
+
+
+# for loop
+if "ຜັກຂະແຍງ" in fruit:
+    print('Have')
+else:
+    print('No have')
+
+# check bool
+print("ຜັກຂະແຍງ" in fruit)
+print("ຜັກຂະແຍງ" not in fruit)
+
+# remove
+fruit.remove('ໝາກຍົມ')
+print(fruit)
+
+# remove discard
+fruit.discard('ຜັກຂະແຍງ')
 print(fruit)
 
 
 # constructor
-lists = ['ປາເຂັງ', 'ປາໄນ']
+# lists = ['ປາເຂັງ', 'ປາໄນ']
 
-fishes = set(['ປານິນ', 'ປານິນ', 'ປາປາກ', 'ປາກຂາວມັນ'])
-fishes = set(lists)
-print(fishes)
-
+# fishes = set(['ປານິນ', 'ປານິນ', 'ປາປາກ', 'ປາກຂາວມັນ'])
+# fishes = set(lists)
+# print(fishes)
