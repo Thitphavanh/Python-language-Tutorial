98e52f829903cf02ada6243c35178427a7ec582a
Hery <thitphavanh.psv@gmail.com>
2022-07-27 06:45:14 +0700

    ອັພເດດ

---
+++ Python Phase2/EP41.py
@@ -0,0 +1,17 @@
+# settutorial
+# Namal
+fruit = {'ໝາກຂາມ', 'ໝາກມ່ວງ', 'ໝາກຍົມ', 3.14, 100, True}
+# add element
+fruit.add('ໝາກທຸລຽນ')
+fruit.add('ໝາກນັດ')
+fruit.add('ໝາກຂຽບ')
+print(fruit)
+
+
+# constructor
+lists = ['ປາເຂັງ', 'ປາໄນ']
+
+fishes = set(['ປານິນ', 'ປານິນ', 'ປາປາກ', 'ປາກຂາວມັນ'])
+fishes = set(lists)
+print(fishes)
+
