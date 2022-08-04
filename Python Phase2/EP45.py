9323b8e4370e1d05be9715e805740b639d871fc5
Hery <thitphavanh.psv@gmail.com>
2022-08-04 07:27:26 +0700

    update

---
+++ Python Phase2/EP45.py
@@ -0,0 +1,11 @@
+# dictionary => key (ການເຂົ້າເຖິງຂໍ້ມູນ), value (ຄ່າຂອງຂໍ້ມູນ)
+
+colors = {"red": "ສີແເດງ", "yellow": "ສີເຫຼືຶອງ", "blue": "ສີຟ້າ", 3.14: "ຄ່າ Pi"}
+country = {"England": "ອັງກິດ"}
+footballers = {"David Beckham": "ນັກເຕະບານ ສັນຊາດ ອັງກິດ"}
+livescore = {2: "Liverpool"}
+
+print(colors[3.14])
+print(country["England"])
+print(footballers["David Beckham"])
+print(livescore[2])
