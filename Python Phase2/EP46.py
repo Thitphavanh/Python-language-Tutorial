d2e055a890f79566cdefffe2b3a8b5c784908a9c
hugging_face_user_name <hugging_face_email>
2022-08-22 06:45:34 +0700

    update

---
+++ Python Phase2/EP46.py
@@ -0,0 +1,18 @@
+# ສາເຫດທີ່ເຮົາຕ້ອງຂຽນ function
+
+a, b, c = 10, 25, 45
+
+if a % 2 == 0:
+    print('ເລກຄູ່')
+else:
+    print('ເລກຄີກ')
+    
+if b % 2 == 0:
+    print('ເລກຄູ່')
+else:
+    print('ເລກຄີກ')
+    
+if c % 2 == 0:
+    print('ເລກຄູ່')
+else:
+    print('ເລກຄີກ')
