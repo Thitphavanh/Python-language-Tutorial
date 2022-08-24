b3b9d51ab07300e061840ac2865df47fc31c8070
hugging_face_user_name <hugging_face_email>
2022-08-24 07:06:29 +0700

    update

--- Python Phase2/EP52.py
+++ Python Phase2/EP52.py
@@ -1,9 +1,26 @@
 # Function *agrs
+# def add(*args):
+#     print(args)
+
+
+# def add(*args):
+#     print(args[0]+args[1])
+
+# def add(*args):
+#     sum = 0
+#     for item in args:
+#         sum += item
+#     print(sum)
+
+
 def add(*args):
-    print(args)
+    sum = 0
+    for i in range(len(args)):
+        sum += args[i]
+    print(sum)
 
 
-add()
-add(10)
+# add()
+add(10, 20)
 add(20, 30)
 add(20, 30, 50, 100, 125, 250, 1000)
