18be68108d30cc55b0b0bfb7df18916399635089
Thitphavanh <thitphavanh@gmail.com>
2022-09-22 07:20:59 +0700

    Exception

---
+++ Python Phase3/EP1.py
@@ -0,0 +1,6 @@
+# Exception
+
+number1 = int(input('ປ້ອນໂຕເລກ 1 : '))
+number2 = int(input('ປ້ອນໂຕເລກ 2 : '))
+result = number1/number2
+print(result)
---
+++ Python Phase3/EP2.py
@@ -0,0 +1,15 @@
+# Exception
+
+# number1 = int(input('ປ້ອນໂຕເລກ 1 : '))
+# number2 = int(input('ປ້ອນໂຕເລກ 2 : '))
+# result = number1/number2
+# print(result)
+
+'''
+try:
+    ຄຳສັ່ງງຣັນໂປຣແກຣມປົກກະຕິ
+except:
+    ຄຳສັ່ງທີ່ເຮັດວຽກຕອນໂປຣແກຣມມີຂໍ້ຜິດພາດ    
+'''
+
+    
