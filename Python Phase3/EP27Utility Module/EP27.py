55859a612a6b825ff9679d35404345aa29c556e6
Thitphavanh <thitphavanh@gmail.com>
2022-11-04 08:03:53 +0700

    update

--- Python Phase3/EP27Utility Module/EP27.py
+++ Python Phase3/EP27Utility Module/EP27.py
@@ -13,3 +13,21 @@ print('ເລີ່ມຕົ້ນ', result)
 print(result.strftime('%x'))  # m/d/y ແບບສັ້ນ
 print(result.strftime('%X'))  # ເວລາ time
 print(result.strftime('%c'))  # ວັນ/ເດືອນ/ວັນທີ ເວລາ ປີ
+
+# ເວລາ
+print(result.strftime("%H:%M:%S %p"))
+
+# 1 - 366
+print(result.strftime('%j'))
+
+# date
+print(result.strftime('%a'))  # ແບບຫຍໍ້
+print(result.strftime('%A'))  # ແບບເຕັມ
+print(result.strftime('%w')) # 0 = Sunday
+print(result.strftime('%d')) # ວັນທີ
+print(result.strftime('%b')) # ເດືອນແບບຫຍໍ້
+print(result.strftime('%B')) # ເດືອນແບບເຕັມ
+print(result.strftime("%d %A %B %Y"))
+
+# ວ/ດ/ປ
+print(result.strftime('%d/%m/%Y'))
