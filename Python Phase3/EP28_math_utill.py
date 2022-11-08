0f007254b8f940c54cec0900c0beb7c3cd5049c1
Thitphavanh <thitphavanh@gmail.com>
2022-11-08 07:59:10 +0700

    update

---
+++ Python Phase3/EP28_math_utill.py
@@ -0,0 +1,22 @@
+import math
+
+# result = math.sqrt(64)
+
+# # scores = math.floor(80.8)
+# scores = math.ceil(80.8)
+# print(scores)
+
+# Sin
+convert = math.radians(30)  # degrees -> radians
+x = math.sin(convert)  # radians
+# print(x)
+
+# Cos
+convert = math.radians(30)  # degrees -> radians
+x = math.cos(convert)  # radians
+# print(x)
+
+# Tan
+convert = math.radians(30)  # degrees -> radians
+x = math.tan(convert)  # radians
+print(x)
